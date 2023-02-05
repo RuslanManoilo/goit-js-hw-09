f93368bd4ffd151af8079a793a973c1c257fe171
Ruslan <manoiloruslan@gmail.com>
2023-02-05 02:49:38 +0200

    Task 01 - complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+// console.log(startButton);
+// console.log(stopButton);
+// console.dir(bodyEl);
 
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStopClick);
+
+let timerColor = null;
+
+function onStartClick(event) {
+    // console.dir(event.currentTarget);   
+    timerColor = setInterval(() => {
+        bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }, 1000);
+
+    event.currentTarget.disabled = true;
+};
+
+function onStopClick() {
+    clearInterval(timerColor);
+    // console.log(`Stop!`);
+    startButton.disabled = false;
+};
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
